d7ef118c6453257fa13f4dee83878a73fca7e0a7
Dejice Jacob <d.jacob.1@research.gla.ac.uk>
2017-05-03 16:25:53 +0100

    change license file to add copyright holder name and date

--- setup/add_license.sh
+++ setup/add_license.sh
@@ -3,22 +3,25 @@
 license_file=""
 source_type=""
 dir=$(pwd)
+prog="foobar"
 
 function parse_opts
 {
     OPTIND=1
     echo "options = " $* 
-    while getopts ":chd:f:" options ; 
+    while getopts ":chd:f:l:" options ; 
     do
         case $options in
             f) license_file="$OPTARG" ;;
             c) source_type="[ch]" ; lang="c" ;;
             d) dir="$OPTARG" ;;
+            l) prog="$OPTARG" ;;
             h) echo "add license text to the top of source" ;
                  echo "   options :";
                  echo "   -f ... license file to read text from" ;
                  echo "   -d ... directory to recursively search" ;
                  echo "   -c ... find and add license to .c and .h files" ;
+                 echo "   -l ... name of program" 
                  exit ;;
             :) echo "option -$OPTARG requires argument" ;;
             ?) echo "invalid option -$OPTARG" ;; 
@@ -34,7 +37,9 @@ function add_license
     do
        if ! grep -iq Copyright $file ; 
        then 
+           echo "adding license in ${license_file} ==> ${file}"
            cat ${license_file} ${file} > ${file}.new && mv ${file}.new ${file} 
+           sed -i -e "s/Foobar/$prog/g" $file 
        fi
     done
 }
