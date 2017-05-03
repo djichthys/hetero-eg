4d0aa1a04dc397959df713d50a333d64f73566b9
Dejice Jacob <d.jacob.1@research.gla.ac.uk>
2017-05-03 17:05:41 +0100

    Comment license on top of header files

--- include/api_err.h
+++ include/api_err.h
@@ -1,22 +1,24 @@
-This program is a set of examples about how to use openCL for use in
-heterogeneous programs
-Copyright (C) 2017  Dejice Jacob
-
-
-This file is part of hetero-examples.
-
-hetero-examples is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-hetero-examples is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with hetero-examples.  If not, see <http://www.gnu.org/licenses/>.
+/*!
+ * This program is a set of examples about how to use openCL for use in
+ * heterogeneous programs
+ * Copyright (C) 2017  Dejice Jacob
+ * 
+ * 
+ * This file is part of hetero-examples.
+ * 
+ * hetero-examples is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * hetero-examples is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with hetero-examples.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 
